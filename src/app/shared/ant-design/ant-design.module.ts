c186c4f380fd4e9bb0b29b832bb1270d16a04931
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-05 21:47:13 +0600

    add navigation icons

--- src/app/shared/ant-design/ant-design.module.ts
+++ src/app/shared/ant-design/ant-design.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NzMenuModule} from "ng-zorro-antd/menu";
+import {NzIconModule} from "ng-zorro-antd/icon";
 
-const exports: any = [NzMenuModule];
+const exports: any = [NzMenuModule,NzIconModule];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  exports: []
+  exports: exports
 })
 export class AntDesignModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AntDesignModule } from "./ant-design/ant-design.module";
-import {SharedComponentsModule} from "./shared-components/shared-components.module";
+import { SharedComponentsModule } from "./shared-components/shared-components.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-const exports : any = [AntDesignModule,SharedComponentsModule] ;
+const exports : any = [AntDesignModule,SharedComponentsModule,FlexLayoutModule] ;
 
 @NgModule({
   declarations: [],
