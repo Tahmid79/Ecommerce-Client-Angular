91141171cf4288f6164fd52f967509923ea24d62
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-05 20:51:58 +0600

    shared

---
+++ src/app/shared/ant-design/ant-design.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {NzMenuModule} from "ng-zorro-antd/menu";
+
+const exports: any = [NzMenuModule];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class AntDesignModule { }
