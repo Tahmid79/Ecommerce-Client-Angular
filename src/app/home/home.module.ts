aedbe1a903123cede70a1ca00e3ece5a021a6c14
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-05 23:36:35 +0600

    product card

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeMainComponent } from './home-main/home-main.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ProductCardComponent } from './product-card/product-card.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
--- src/app/shared/ant-design/ant-design.module.ts
+++ src/app/shared/ant-design/ant-design.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NzMenuModule} from "ng-zorro-antd/menu";
 import {NzIconModule} from "ng-zorro-antd/icon";
+import { NzCardModule } from 'ng-zorro-antd/card';
 
-const exports: any = [NzMenuModule,NzIconModule];
+const exports: any = [NzMenuModule,NzIconModule,NzCardModule];
 
 @NgModule({
   declarations: [],
