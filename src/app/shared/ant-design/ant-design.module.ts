6d828b9f863c79467d8a06e315883d4a418c69ac
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-08 23:50:10 +0600

    add spinner

--- src/app/shared/ant-design/ant-design.module.ts
+++ src/app/shared/ant-design/ant-design.module.ts
@@ -4,8 +4,9 @@ import {NzMenuModule} from "ng-zorro-antd/menu";
 import {NzIconModule} from "ng-zorro-antd/icon";
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 
-const exports: any = [NzMenuModule,NzIconModule,NzCardModule,NzGridModule];
+const exports: any = [NzMenuModule,NzIconModule,NzCardModule,NzGridModule,NzSpinModule];
 
 @NgModule({
   declarations: [],
